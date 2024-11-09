33e3a02860b8882e97cae35341a54312e4a154cb
jumpei527 <jumpei5278@gmail.com>
2024-11-09 22:17:01 +0900

    Update 268. Missing Number

--- 0268_missing_number.py
+++ 0268_missing_number.py
@@ -20,3 +20,19 @@ class Solution2:
         for n in nums:
             res -= n
         return res
+
+
+class Solution3:
+    # Time: O(nlogn)
+    # Space: O(1)
+    def missingNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        ok = -1
+        ng = len(nums)
+        while abs(ok - ng) > 1:
+            mid = (ok + ng) // 2
+            if mid == nums[mid]:
+                ok = mid
+            else:
+                ng = mid
+        return ng
