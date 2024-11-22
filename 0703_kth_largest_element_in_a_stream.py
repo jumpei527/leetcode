e057592e9279e8855f5d08737d00e41fc8d82b7a
jumpei527 <jumpei5278@gmail.com>
2024-11-22 20:36:26 +0900

    Add 703. Kth Largest Element in a Stream

---
+++ 0703_kth_largest_element_in_a_stream.py
@@ -0,0 +1,20 @@
+# n = len(nums)
+# Time = O(nlogn)
+# Space = O(n)
+from typing import List
+import heapq
+
+
+class KthLargest:
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.nums = nums
+        heapq.heapify(self.nums)
+        while len(self.nums) > k:
+            heapq.heappop(self.nums)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.nums, val)
+        if len(self.nums) > self.k:
+            heapq.heappop(self.nums)
+        return self.nums[0]
