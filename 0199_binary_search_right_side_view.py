ff2e908a65573744e269631546592531bc7a4eb3
jumpei527 <jumpei5278@gmail.com>
2025-01-11 16:01:56 +0900

    Update 199. Binary Tree Right Side View

--- 0199_binary_search_right_side_view.py
+++ 0199_binary_search_right_side_view.py
@@ -1,6 +1,6 @@
 # n: node_count
 # Time: O(n)
-# Space: O(n/2)
+# Space: O(n)
 from collections import deque
 from typing import Optional, List
 
@@ -18,17 +18,17 @@ class Solution:
         if not root:
             return []
         ans = []
-        stack = deque([root])
+        queue = deque([root])
 
-        while stack:
-            depth = len(stack)
-            for i in range(depth):
-                cur = stack.popleft()
+        while queue:
+            length = len(queue)
+            for i in range(length):
+                cur = queue.popleft()
                 if cur.left:
-                    stack.append(cur.left)
+                    queue.append(cur.left)
                 if cur.right:
-                    stack.append(cur.right)
-                if i == depth - 1:
+                    queue.append(cur.right)
+                if i == length - 1:
                     ans.append(cur.val)
 
         return ans
