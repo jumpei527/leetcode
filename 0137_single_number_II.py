f26da4dc2eea41a3da3b7f450d480df4ab714ba4
jumpei527 <jumpei5278@gmail.com>
2025-03-17 16:08:47 +0900

    Add 137. Single Number II

---
+++ 0137_single_number_II.py
@@ -0,0 +1,23 @@
+# n = len(nums)
+# Time: O(n)
+# Space: O(1)
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ans = 0
+
+        for i in range(32):
+            bit_sum = 0
+            for num in nums:
+                if num < 0:
+                    num = num & (2**32-1)
+                bit_sum += (num >> i) & 1
+            bit_sum %= 3
+            ans |= bit_sum << i
+
+        if ans >= 2**31:
+            ans -= 2**32
+
+        return ans
