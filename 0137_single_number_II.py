4ad813e55adda7f3d842c0e030d90c65c42c5673
jumpei527 <jumpei5278@gmail.com>
2025-04-02 19:14:21 +0900

    Update 137. Single Number II

--- 0137_single_number_II.py
+++ 0137_single_number_II.py
@@ -2,22 +2,80 @@
 # Time: O(n)
 # Space: O(1)
 from typing import List
+import unittest
+from returns.result import Result, Failure, Success
 
 
 class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        ans = 0
+    def singleNumber(self, nums: List[int]) -> Result[str, Exception]:
+        """
+        Given a list of integers where each integer appears three times
+        except for one integer which appears once, find that single
+        integer.
 
-        for i in range(32):
+        Args:
+            nums: A list of integers where each integer appears three
+            times except for one integer which appears once.
+        Returns:
+            int: The integer that appears only once in the list.
+        Examples:
+            >>> Solution().singleNumber([2, 2, 3, 2])
+            Success(3)
+        """
+        if not nums:
+            return Failure("The list is empty.")
+
+        INT_MAX = 2**31
+        UINT_MAX = 2**32
+
+        single_num = self.build_single_number_from_bits(nums)
+
+        # If the number is negative, we need to convert it to a signed integer
+        if single_num >= INT_MAX:
+            single_num -= UINT_MAX
+
+        return Success(single_num)
+
+    def build_single_number_from_bits(self, nums: List[int]) -> int:
+        """
+        Build the single number from the bits of the numbers in the list.
+
+        Args:
+            nums: A list of integers where each integer appears three
+            times except for one integer which appears once.
+        Returns:
+            int: The integer that appears only once in the list.
+        Examples:
+            >>> Solution().build_single_number_from_bits([2, 2, 3, 2])
+            3
+        """
+        BIT_LENGTH = 32
+        REPEAT_COUNT = 3
+        BIT_MASK = 1
+
+        single_num = 0
+
+        for bit_idx in range(BIT_LENGTH):
             bit_sum = 0
             for num in nums:
-                if num < 0:
-                    num = num & (2**32-1)
-                bit_sum += (num >> i) & 1
-            bit_sum %= 3
-            ans |= bit_sum << i
+                bit_sum += (num >> bit_idx) & BIT_MASK
+            bit_sum %= REPEAT_COUNT
+            single_num |= bit_sum << bit_idx
 
-        if ans >= 2**31:
-            ans -= 2**32
+        return single_num
 
-        return ans
+
+class TestSingleNumber(unittest.TestCase):
+    def test_singleNumber_negative_number(self):
+        expected = Success(-1)
+        result = Solution().singleNumber([-2, -2, -1, -2])
+        self.assertEqual(
+            result, expected, f"Expected {expected} but got {result}"
+        )
+
+    def test_singleNumber_empty_list(self):
+        expected = Failure("The list is empty.")
+        result = Solution().singleNumber([])
+        self.assertEqual(
+            result, expected, f"Expected {expected} but got {result}"
+        )
