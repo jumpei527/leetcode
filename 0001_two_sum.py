2add38be1e0bed4b67f7e942c1084201174afc2b
jumpei527 <jumpei5278@gmail.com>
2025-10-05 14:52:03 +0900

    Add 1. Two Sum

---
+++ 0001_two_sum.py
@@ -0,0 +1,14 @@
+# n = len(nums)
+# Time: O(n)
+# Space: O(n)
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_index = {}
+        for idx, num in enumerate(nums):
+            if target - num in num_index:
+                return [num_index[target - num], idx]
+
+            num_index[num] = idx
