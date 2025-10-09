102d59de8e5a41e8357ad06f2ddc32e5342f86a0
jumpei527 <jumpei5278@gmail.com>
2025-10-09 14:07:52 +0900

    Update 238. Product of Array Except Self

--- 0238_product_of_array_except_self.py
+++ 0238_product_of_array_except_self.py
@@ -6,25 +6,15 @@ from typing import List
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        total_product = 1
-        zero_cnt = 0
-        for n in nums:
-            if n == 0:
-                zero_cnt += 1
-            else:
-                total_product *= n
+        ans = [0] * len(nums)
+        prefix_product = 1
+        for idx in range(len(nums)):
+            ans[idx] = prefix_product
+            prefix_product *= nums[idx]
 
-        if zero_cnt >= 2:
-            for i in range(len(nums)):
-                nums[i] = 0
-            return nums
+        suffix_product = 1
+        for idx in range(len(nums) - 1, -1, -1):
+            ans[idx] *= suffix_product
+            suffix_product *= nums[idx]
 
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                nums[i] = total_product
-            elif zero_cnt == 1:
-                nums[i] = 0
-            else:
-                nums[i] = total_product // nums[i]
-
-        return nums
+        return ans
