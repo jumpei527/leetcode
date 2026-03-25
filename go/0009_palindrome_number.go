59d9c402fa7ab95597519aa288185bbb90cc6dfd
jumpei527 <jumpei5278@gmail.com>
2026-03-25 23:50:14 +0900

    [Go] Add 9. Palindrome Number

---
+++ go/0009_palindrome_number.go
@@ -0,0 +1,21 @@
+package main
+
+// n = number of digits
+// Time:  O(n)
+// Space: O(1)
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+
+	original := x
+	reversed := 0
+
+	for x != 0 {
+		digit := x % 10
+		reversed = reversed*10 + digit
+		x /= 10
+	}
+
+	return original == reversed
+}
