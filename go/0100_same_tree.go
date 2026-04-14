a0eeb1aa080d988d5ca4d1ef33733cfd6fcb4314
jumpei527 <jumpei5278@gmail.com>
2026-04-14 21:10:12 +0900

    [Go] Add 100. Same Tree

---
+++ go/0100_same_tree.go
@@ -0,0 +1,23 @@
+package main
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// n = number of nodes in the tree
+// Time:  O(n)
+// Space: O(n)
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil || q == nil {
+		return p == q
+	}
+
+	if p.Val == q.Val && isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right) {
+		return true
+	}
+
+	return false
+}
