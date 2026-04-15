1b3e86b1038d92b7c61b38b871712fb7bbf4cba3
jumpei527 <jumpei5278@gmail.com>
2026-04-15 17:43:24 +0900

    [Go] Add 94. Binary Tree Inorder Traversal

---
+++ go/0094_binary_tree_inorder_traversal.go
@@ -0,0 +1,31 @@
+package main
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// n = number of nodes in the tree
+// Time:  O(n)
+// Space: O(n)
+func inorderTraversal(root *TreeNode) []int {
+	result := []int{}
+
+	var inorder func(*TreeNode)
+
+	inorder = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+
+		inorder(root.Left)
+		result = append(result, root.Val)
+		inorder(root.Right)
+	}
+
+	inorder(root)
+
+	return result
+}
