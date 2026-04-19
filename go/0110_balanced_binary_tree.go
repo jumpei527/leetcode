8b525023f8dc22ab65ce0252683db8044e741746
jumpei527 <jumpei5278@gmail.com>
2026-04-19 12:11:56 +0900

    [Go] Add 110. Balanced Binary Tree

---
+++ go/0110_balanced_binary_tree.go
@@ -0,0 +1,37 @@
+package main
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// n = number of nodes in the tree
+// Time:  O(n)
+// Space: O(n)
+func isBalanced(root *TreeNode) bool {
+	var checkBalance func(*TreeNode) (int, bool)
+
+	checkBalance = func(root *TreeNode) (int, bool) {
+		if root == nil {
+			return 0, true
+		}
+
+		leftHeight, leftOk := checkBalance(root.Left)
+		rightHeight, rightOk := checkBalance(root.Right)
+		if !leftOk || !rightOk {
+			return 0, false
+		}
+
+		if leftHeight-rightHeight > 1 || rightHeight-leftHeight > 1 {
+			return 0, false
+		}
+
+		return max(leftHeight, rightHeight) + 1, true
+	}
+
+	_, balanced := checkBalance(root)
+	return balanced
+
+}
